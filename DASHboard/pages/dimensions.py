d7ef9c9c8e9cd54bfb8404d1119c58f714059e60
ajg54 <alexisgillett@hotmail.com>
2022-09-11 09:53:08 +0200

    Made the dimensions.py slide.

--- DASHboard/pages/dimensions.py
+++ DASHboard/pages/dimensions.py
@@ -1,6 +1,9 @@
 from dash import dcc, html, Input, Output, callback
 
 layout = html.Div([
+    html.H2("Latent profile and latent class analysis"),
+    html.Img(src="assets/01_EGA_Results.png", width="100%"),
+    html.Img(src="assets/03_LPA.png", width="100%"),
     html.Br(),
     dcc.Link('Next page', href='/population_bias')
 ])
