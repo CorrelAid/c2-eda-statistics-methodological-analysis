52fabd86e46dc091bd9845aadd31ea043558857a
ajg54 <alexisgillett@hotmail.com>
2022-09-11 09:30:01 +0200

    Emptied the dimensions.py page so that it is ready for the actual content to be added.

--- DASHboard/pages/dimensions.py
+++ DASHboard/pages/dimensions.py
@@ -1,12 +1,7 @@
 from dash import dcc, html, Input, Output, callback
 
 layout = html.Div([
-    html.H2("Motivating Questions"),
-    html.P("Which statistical analyses make sense with our data?"),
-    html.P("Which methods can be used to derive which statements from the data?"),
-    html.H2("Our Initial Solution Space"),
-    html.Img(src="https://www.reactiongifs.com/r/weeds.gif", width="100%"),
     html.Br(),
-    dcc.Link('Next page', href='/problem_definition')
+    dcc.Link('Next page', href='/population_bias')
 ])
 
