b5754bfad515d31a2a3ccdcc015f6c47cb9ccd66
ajg54 <alexisgillett@hotmail.com>
2022-09-11 09:13:36 +0200

    Cleaned up the problem_definition.py slide with an overview of our three topics.

--- DASHboard/pages/problem_definition.py
+++ DASHboard/pages/problem_definition.py
@@ -1,9 +1,25 @@
 from dash import dcc, html, Input, Output, callback
 import dash_bootstrap_components as dbc
 
-tab_eda = [html.P("Exploratory Data Analysis vs. Confirmatory Data Analysis")]
-tab_bias = [html.P("Population and Bias")]
-tab_dimension = [html.P("Dimensionality Reduction")]
+tab_eda = [html.Br(),
+           html.P("Exploratory Data Analysis vs. Confirmatory Data Analysis."),
+           html.Ul([
+               html.Li("The distinction"),
+               html.Li("Pitfalls"),
+               html.Li("Remedy")
+           ])]
+tab_dimension = [html.Br(),
+                 html.P("Dimensionality Reduction"),
+                 html.Ul([
+                     html.Li("A classic EDA technique"),
+                     html.Li("Making data more manageable")
+                 ])]
+tab_bias = [html.Br(),
+            html.P("Population and Bias"),
+            html.Ul([
+                html.Li("Correcting for sampling bias needed for CDA"),
+                html.Li("Related topics of bias and comparison to other populations.")
+            ])]
 
 layout = html.Div([
     html.H2("Example Questions"),
@@ -12,10 +28,11 @@ layout = html.Div([
     html.P("What are the implications of sample bias that we need to keep in mind / "
            "discuss when drawing conclusions from the data?"),
     html.H2("Our Sub-questions"),
+    html.P("From an initial brainstorm, we came up with three related topics."),
     dbc.Tabs([
         dbc.Tab(tab_eda, label="EDA vs CDA"),
-        dbc.Tab(tab_bias, label="Population and Bias"),
-        dbc.Tab(tab_dimension, label="Dimensionality Reduction")
-    ])
-    ]
-)
+        dbc.Tab(tab_dimension, label="Dimensionality Reduction"),
+        dbc.Tab(tab_bias, label="Population and Bias")
+    ]),
+    dcc.Link('Next page', href='/dimensionality')
+])
